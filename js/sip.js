53f5eabff49d9b2023b255b36c78289f52260161
bablu-fintech <customercare.investmentsarathi@gmail.com>
2026-08-02 00:48:52 +0530

    Added Calculator Tabs UI

--- js/sip.js
+++ js/sip.js
@@ -43,3 +43,46 @@ function calculateSIP() {
         });
 
 }
+// =========================================
+// Calculator Tab Switching
+// =========================================
+
+const tabs = document.querySelectorAll(".tab-btn");
+
+const sipSection = document.querySelector(".calculator-wrapper");
+
+const lumpSection = document.getElementById("lumpsumCalculator");
+
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(btn => btn.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        const type = tab.dataset.tab;
+
+        if (type === "sip") {
+
+            sipSection.style.display = "grid";
+            lumpSection.style.display = "none";
+
+        }
+
+        if (type === "lumpsum") {
+
+            sipSection.style.display = "none";
+            lumpSection.style.display = "grid";
+
+        }
+
+        if (type === "swp") {
+
+            alert("SWP Calculator Coming Soon");
+
+        }
+
+    });
+
+});
