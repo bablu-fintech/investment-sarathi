3137d148ba3b31586000603672220f83dc01732a
bablu-fintech <customercare.investmentsarathi@gmail.com>
2026-08-03 01:56:44 +0530

    feat: redesign homepage hero and dashboard UI

--- js/home.js
+++ js/home.js
@@ -1,1 +1,46 @@
 console.log("Investment Sarathi Home Loaded");
+
+const ctx = document.getElementById("portfolioChart");
+
+if (ctx) {
+
+    new Chart(ctx, {
+
+        type: "line",
+
+        data: {
+            labels: ["Jan","Feb","Mar","Apr","May","Jun"],
+
+            datasets: [{
+                label: "Portfolio",
+                data: [4,5,5.5,6.2,7.1,8],
+                borderColor: "#2563EB",
+                backgroundColor: "rgba(37,99,235,.15)",
+                fill: true,
+                tension: 0.4
+            }]
+        },
+
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+
+            scales: {
+                y: {
+                    display: false
+                },
+                x: {
+                    display: false
+                }
+            }
+        }
+
+    });
+
+}
